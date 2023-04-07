eb9c07294cd9043439b63d708b636e0841bcaed7
SUNDUS SIDDIQUI <126038622+Sundusgithub@users.noreply.github.com>
2023-04-07 23:16:07 +0500

    Add files via upload
    Done

---
+++ ZAKAT.js
@@ -0,0 +1,7 @@
+let zakaatPer = 0.025;
+let userInput;
+userInput =  Number(prompt("Enter Your Zakaat Value"));
+let result = zakaatPer * userInput;
+//alert('your zakaat value is ${result}');
+alert("Your Zakaat Value Is "+result);
+//document.write('your Zakaat Value Is '+result);
